266915804b16050bb04d06d2f0c52c2f22747e31
ajjude <ajjude@gmail.com>
2021-01-04 12:50:57 +0530

    instance code updated

--- instance.tf
+++ instance.tf
@@ -1,7 +1,6 @@
 provider "aws"{
- region          = "ap-south-1"
- access_key      = "AKIAYW36XQKVSJPWH2GD"
- secret_key      = "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"
+ region          = var.region
+ profile         = var.profile
  version         = "~> 2.0"
  }
 
@@ -9,4 +8,3 @@ resource "aws_instance" "testinstance" {
  ami = "ami-024615f49be85226a"
  instance_type = "t2.micro"
  }
-
